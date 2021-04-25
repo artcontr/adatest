8449985e4f5ab46880a64ee9286fb1674195aa2c
artcontr <artcontr@gmail.com>
2021-04-25 21:38:35 +0200

    rgbw

--- neopixel_rpi_simpletest.py
+++ neopixel_rpi_simpletest.py
@@ -12,11 +12,11 @@ import neopixel
 pixel_pin = board.D18
 
 # The number of NeoPixels
-num_pixels = 30
+num_pixels = 50
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
-ORDER = neopixel.GRB
+ORDER = neopixel.GRBW
 
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
