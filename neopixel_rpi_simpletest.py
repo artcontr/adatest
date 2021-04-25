9d27eeec590f7ee87a4fb8746ed157e6415470c3
artcontr <artcontr@gmail.com>
2021-04-25 21:49:48 +0200

    Update neopixel_rpi_simpletest.py

--- neopixel_rpi_simpletest.py
+++ neopixel_rpi_simpletest.py
@@ -58,21 +58,21 @@ while True:
     # Comment this line out if you have RGBW/GRBW NeoPixels
     #pixels.fill((255, 0, 0))
     #Uncomment this line if you have RGBW/GRBW NeoPixels
-     pixels.fill((255, 0, 0, 0))
+    pixels.fill((255, 0, 0, 0))
     pixels.show()
     time.sleep(1)
 
     # Comment this line out if you have RGBW/GRBW NeoPixels
     #pixels.fill((0, 255, 0))
     # Uncomment this line if you have RGBW/GRBW NeoPixels
-     pixels.fill((0, 255, 0, 0))
+    pixels.fill((0, 255, 0, 0))
     pixels.show()
     time.sleep(1)
 
     # Comment this line out if you have RGBW/GRBW NeoPixels
     #pixels.fill((0, 0, 255))
     #Uncomment this line if you have RGBW/GRBW NeoPixels
-     pixels.fill((0, 0, 255, 0))
+    pixels.fill((0, 0, 255, 0))
     pixels.show()
     time.sleep(1)
 
